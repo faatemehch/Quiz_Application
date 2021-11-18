fc206531794a95216db9d6b38f9f4bb09c1da626
faatemeh chamani <fch199674@yahoo.com>
2021-11-18 09:37:37 +0330

    checking view is updated

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,8 @@
+from django.contrib.auth.models import User
 from django.http import JsonResponse
-from django.shortcuts import render
-from .models import Quiz, Question
+from django.shortcuts import render, redirect
+from .models import Quiz, Question, Result
 from django.views.generic import ListView
-import random
 
 
 class QuizListView( ListView ):
@@ -19,14 +19,20 @@ def quizView(request, *args, **kwargs):
 
 
 def checkingView(request, *args, **kwargs):
-    quiz: Quiz = Quiz.objects.filter( id=kwargs['quiz_id'] ).first()
-    # questions: Question = quiz.question_set.all()
-    questions: Question = Question.objects.filter( quiz_id=kwargs['quiz_id'] ).all()
+    if request.POST:
+        questions: Question = Question.objects.filter( quiz_id=kwargs['quiz_id'] ).all()
+        user = User.objects.filter( id=request.user.id ).first()
 
-    number_of_correct_questions = 0
-    for num, question in enumerate( questions ):
-        real_answer = question.get_correct()
-        user_answer = request.GET.get( f'{question.question_text}' )
-        if real_answer == user_answer:
-            number_of_correct_questions += 1
-    return render( request, 'quiz/checking.html', {'score': number_of_correct_questions} )
+        form_data = dict( request.POST )
+        form_data.pop( 'csrfmiddlewaretoken' )
+
+        number_of_correct_questions = 0
+        for num, question in enumerate( questions ):
+            real_answer = question.get_correct()
+            user_answer = request.POST.get( f'{question.question_text}' )
+            if real_answer == user_answer:
+                number_of_correct_questions += 1
+        score = (number_of_correct_questions / len( questions )) * 100
+        Result.objects.create( quiz_id=kwargs['quiz_id'], owner=user, score=score )
+        return render( request, 'quiz/checking.html', {'score': score} )
+    return redirect( 'quiz:home-view' )
